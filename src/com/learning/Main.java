1eb51beb8c554679a370c2d7955e04fdc22067c9
mary <melina.m2011@gmail.com>
2022-07-03 06:18:34 +0300

    init challenge #1

--- src/com/learning/Main.java
+++ src/com/learning/Main.java
@@ -1,8 +1,82 @@
 package com.learning;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Welcome to user management Application.");
+        showMenu();
     }
+
+    public static void showMenu() {
+        Scanner scanner = new Scanner(System.in);
+        String message ="please enter a number between 1 to 4. \n";
+
+        int actionNumber = 0;
+        while (actionNumber != 4) {
+//            clearConsole();
+            System.out.println(message +
+                    "[1] ADD new user. \n" +
+                    "[2] Edit user. \n" +
+                    "[3] Remove user. \n" +
+                    "[4] Exit. \n" +
+                    "what is your action?");
+
+            try {
+                actionNumber = scanner.nextInt();
+                switch (actionNumber) {
+                    case 1:
+                        addNewUser();
+                        break;
+                    case 2:
+                        editUser();
+                        break;
+                    case 3:
+                        removeUser();
+                        break;
+                    case 4:
+                        break;
+                    default:
+                        System.out.println(message);
+                        break;
+                }
+
+            } catch (InputMismatchException e) {
+                System.out.println(message);
+                scanner.next();
+            }
+        }
+    }
+
+    private static void removeUser() {
+        //TODO
+        System.out.println("removed a user");
+    }
+
+    private static void editUser() {
+        //TODO
+        System.out.println("edited a user");
+    }
+
+    private static void addNewUser() {
+        //TODO
+        System.out.println("added a user");
+    }
+
+//    public static void clearConsole() {
+//        try {
+//            String operatingSystem = System.getProperty("os.name");
+//
+//            if (operatingSystem.contains("Windows")) {
+//                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+//            } else {
+//                new ProcessBuilder("clear").inheritIO().start().waitFor();
+//            }
+//        } catch (Exception e) {
+//            System.out.println(e);
+//        }
+//    }
+
 }
---
+++ test/com/learning/MainTest.java
@@ -0,0 +1,13 @@
+package com.learning;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void showMenu() {
+
+    }
+}
